1d6661b58b7ac11e9a7751d5feb0665b21a7811f
wpinedaCrack <wapcamargo@gmail.com>
2025-03-20 19:29:32 -0500

    Autorizacion permisos y Roles Segunda Parte

--- src/CleanArchitecture/CleanArchitecture.Domain/Roles/Role.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Roles/Role.cs
@@ -6,7 +6,7 @@ namespace CleanArchitecture.Domain.Roles;
 public sealed class Role : Enumeration<Role>
 {
     public static readonly Role Cliente = new(1, "Cliente");
-    public static readonly Role Admin = new(1, "Admin");
+    public static readonly Role Admin = new(2, "Admin");
 
     public Role(int id, string name) : base(id, name)
     {
---
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchitecture.Domain.Permissions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infrastructure.Configurations;
+
+public sealed class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+{
+    public void Configure(EntityTypeBuilder<Permission> builder)
+    {
+        builder.ToTable("permissions");
+        builder.HasKey(p => p.Id);
+
+        builder.Property(x => x.Id)
+        .HasConversion(
+            permissionId => permissionId!.Value,
+            value => new PermissionId(value)
+        );
+
+        builder.Property(x => x.Nombre)
+        .HasConversion(
+            permissionNombre => permissionNombre!.Value,
+            value => new Nombre(value)
+        );
+
+        IEnumerable<Permission> permissions = Enum.GetValues<PermissionEnum>()
+        .Select(p => new Permission(
+            new PermissionId((int)p),
+            new Nombre(p.ToString())
+        ));
+
+        builder.HasData(permissions);
+    }
+}
